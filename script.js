70cb1b5f25199132e0cf759ce6fc781d6f4865a9
lsisneros <58485272+lsisneros@users.noreply.github.com>
2019-12-04 14:08:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -381,5 +381,25 @@ function keyEvent(e) {
 }
 
 function tileCount() {
-    	alert(next-1);    
+    	alert("Tiles used:  " next-1);    
+}
+
+function touch2Mouse(e)
+{
+  var theTouch = e.changedTouches[0];
+  var mouseEv;
+
+  switch(e.type)
+  {
+    case "touchstart": mouseEv="mousedown"; break;  
+    case "touchend":   mouseEv="mouseup"; break;
+    case "touchmove":  mouseEv="mousemove"; break;
+    default: return;
+  }
+
+  var mouseEvent = document.createEvent("MouseEvent");
+  mouseEvent.initMouseEvent(mouseEv, true, true, window, 1, theTouch.screenX, theTouch.screenY, theTouch.clientX, theTouch.clientY, false, false, false, false, 0, null);
+  theTouch.target.dispatchEvent(mouseEvent);
+
+  e.preventDefault();
 }
